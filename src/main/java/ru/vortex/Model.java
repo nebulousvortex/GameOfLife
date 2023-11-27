f32b4440b25d9098acbb2a80f560466f5c3ea7ac
NebulousVortex <VortexOfNebula@mail.ru>
2023-11-28 00:14:59 +0300

    Removed unnecessary static methods

--- src/main/java/ru/vortex/Model.java
+++ src/main/java/ru/vortex/Model.java
@@ -35,18 +35,7 @@ public class Model {
     /*
      * Generate first generation of cells
      */
-    public static Cell[][] makeFirstGen(){
-        Cell[][] cells = new Cell[][]{
-                {new Cell(0, 0, true),new Cell(0, 1, true),new Cell(0, 2, false),new Cell(0, 3, true),new Cell(0, 4, true)},
-                {new Cell(1, 0, true),new Cell(1, 1, false),new Cell(1, 2, true),new Cell(1, 3, false),new Cell(1, 4, true)},
-                {new Cell(2, 0, true),new Cell(2, 1, false),new Cell(2, 2, false),new Cell(2, 3, false),new Cell(2, 4, true)},
-                {new Cell(3, 0, true),new Cell(3, 1, true),new Cell(3, 2, false),new Cell(3, 3, true),new Cell(3, 4, true)},
-                {new Cell(4, 0, false),new Cell(4, 1, false),new Cell(4, 2, false),new Cell(4, 3, false),new Cell(4, 4, false)},
-                {new Cell(5, 0, false),new Cell(5, 1, false),new Cell(5, 2, false),new Cell(5, 3, false),new Cell(5, 4, false)},
-                {new Cell(6, 0, false),new Cell(6, 1, false),new Cell(6, 2, false),new Cell(6, 3, false),new Cell(6, 4, false)},
-                {new Cell(7, 0, false),new Cell(7, 1, false),new Cell(7, 2, false),new Cell(7, 3, false),new Cell(7, 4, false)},
-                {new Cell(8, 0, false),new Cell(8, 1, false),new Cell(8, 2, false),new Cell(8, 3, false),new Cell(8, 4, false)}
-        };
+    public Cell[][] makeFirstGen(){
 //        // На случай доработки в виде ввода в консоль
 //        Scanner scanner = new Scanner(System.in);
 //        Cell[][] cells = new Cell[size+3][size];
@@ -57,13 +46,23 @@ public class Model {
 //            }
 //
 //        }
-        return cells;
+        return new Cell[][]{
+                {new Cell(0, 0, true),new Cell(0, 1, true),new Cell(0, 2, false),new Cell(0, 3, true),new Cell(0, 4, true)},
+                {new Cell(1, 0, true),new Cell(1, 1, false),new Cell(1, 2, true),new Cell(1, 3, false),new Cell(1, 4, true)},
+                {new Cell(2, 0, true),new Cell(2, 1, false),new Cell(2, 2, false),new Cell(2, 3, false),new Cell(2, 4, true)},
+                {new Cell(3, 0, true),new Cell(3, 1, true),new Cell(3, 2, false),new Cell(3, 3, true),new Cell(3, 4, true)},
+                {new Cell(4, 0, false),new Cell(4, 1, false),new Cell(4, 2, false),new Cell(4, 3, false),new Cell(4, 4, false)},
+                {new Cell(5, 0, false),new Cell(5, 1, false),new Cell(5, 2, false),new Cell(5, 3, false),new Cell(5, 4, false)},
+                {new Cell(6, 0, false),new Cell(6, 1, false),new Cell(6, 2, false),new Cell(6, 3, false),new Cell(6, 4, false)},
+                {new Cell(7, 0, false),new Cell(7, 1, false),new Cell(7, 2, false),new Cell(7, 3, false),new Cell(7, 4, false)},
+                {new Cell(8, 0, false),new Cell(8, 1, false),new Cell(8, 2, false),new Cell(8, 3, false),new Cell(8, 4, false)}
+        };
     }
 
     /*
      * Generate next generation of cells
      */
-    public static void makeNextGen(Cell[][] cells){
+    public void makeNextGen(Cell[][] cells){
         for(Cell[] cellRow: cells){
             for(Cell cell: cellRow){
                 cell.setCountNeighbours(cells);
@@ -79,7 +78,7 @@ public class Model {
     /*
      * Print current generation of cells
      */
-    public static void printGen(Cell[][] cells){
+    public void printGen(Cell[][] cells){
         for(Cell[] cellRow: cells){
             for(Cell cell: cellRow){
                 System.out.print(cell.getValue());
